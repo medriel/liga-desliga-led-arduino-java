5fff811a6cef3b18e0a160090d045a0718ec23ac
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-29 11:46:34 -0300

    Implementando botão de liga/desliga

--- src/javafxarduino/FXMLDocumentController.java
+++ src/javafxarduino/FXMLDocumentController.java
@@ -1,6 +1,7 @@
 package javafxarduino;
 
 import com.fazecast.jSerialComm.SerialPort;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -9,42 +10,57 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private ComboBox cbPortas;
-    
+
     @FXML
     private Button btnConectar;
-    
+
     private SerialPort porta;
-    
+    private int led = 0;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         carregarPortas();
-    }    
-    private void carregarPortas(){
+    }
+
+    private void carregarPortas() {
         SerialPort[] portNames = SerialPort.getCommPorts();
-        
-        for(SerialPort portName: portNames){
+
+        for (SerialPort portName : portNames) {
             cbPortas.getItems().add(portName.getSystemPortName());
         }
     }
-    
+
     @FXML
-    private void btnConectarAction (){
-        if(btnConectar.getText().equals("Conectar")){
+    private void btnConectarAction() {
+        if (btnConectar.getText().equals("Conectar")) {
             porta = SerialPort.getCommPort(cbPortas.getSelectionModel().getSelectedItem().toString());
-            if(porta.openPort()){
+            if (porta.openPort()) {
                 btnConectar.setText("Desconectar");
                 cbPortas.setDisable(true);
             }
-        }else{
+        } else {
             porta.closePort();
             cbPortas.setDisable(false);
             btnConectar.setText("Conectar");
         }
-        System.out.println("Teste");
     }
-    
-    
+
+    @FXML
+    private void btnLedAction() {
+        PrintWriter output = new PrintWriter(porta.getOutputStream());
+
+        if (led == 0) {
+            output.print("1");
+            output.flush();
+            led = 1;
+        } else {
+            output.print("0");
+            output.flush();
+            led = 0;
+        }
+
+    }
 }
